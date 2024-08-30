a882fc1998820cb320c108dd08ded56c1adf5a19
sheikhibrahim57 <sheikhibrahimsi57@gmail.com>
2024-08-30 18:12:40 +0600

    created api for get blogs

--- index.js
+++ index.js
@@ -32,6 +32,11 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/getBlog", async (req, res) => {
+      const result = await blogCollection.find({}).toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
